09bf9bcdb214dabfe7441e7ae1b50d99629d8721
Maria Terezinha França <mt3franca@gmail.com>
2023-07-01 18:32:24 -0300

    Update sources, stg_raw_salesorderheader and stg_raw_salesreason

--- models/crm/staging/stg_raw_salesorderheadersalesreason.sql
+++ models/crm/staging/stg_raw_salesorderheadersalesreason.sql
@@ -1,8 +1,8 @@
 with
   salesorderheadersalesreason as (
     select
-      salesorderid as salesorderid_sohsr
-      , salesreasonid as salesreasonid_sohsr
+      salesorderid as salesorderid_sr
+      , salesreasonid as salesreasonid_sr
       , cast(modifieddate as datetime) as modifieddate_sr
     from {{ source('source_adw', 'salesorderheadersalesreason') }}
 )
