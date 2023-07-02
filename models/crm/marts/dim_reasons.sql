ab65f1e14efe112ec2c6fd4baccfccd2b62198c0
Maria Terezinha França <mt3franca@gmail.com>
2023-07-02 11:46:25 -0300

    Update dim_reasons with aggregated reasons from int_reasons

--- models/crm/marts/dim_reasons.sql
+++ models/crm/marts/dim_reasons.sql
@@ -1,26 +1,22 @@
 with
-    salesorderheadersalesreason as (
-        select * 
-        from {{ref('stg_raw_salesorderheadersalesreason')}}
-    )
-
-    , salesreason as (
-        select * 
-        from {{ref('stg_raw_salesreason')}}
-    )
-
-    , reason as (
+    reason_aggregate as (
         select
-            {{ dbt_utils.generate_surrogate_key([
-                'salesorderheadersalesreason.salesorderid_sr'
-                , 'salesorderheadersalesreason.salesreasonid_sr']) }} as reason_sk
-            , salesorderheadersalesreason.salesorderid_sr
-            , salesreason.reasontype
-            , salesorderheadersalesreason.modifieddate_sr    
-        from salesorderheadersalesreason
-        left join salesreason  
-                on salesorderheadersalesreason.salesreasonid_sr = salesreason.salesreasonid
+            salesorderid_sr as reasons_sk
+            , salesorderid_sr
+            , case
+                when reasons in ('Other & Other', 'Other')
+                    then 'Other'
+                when reasons in ('Other & Promotion', 'Other & Promotion & Other', 'Promotion & Other')
+                    then 'Promotion & Other'
+                when reasons = 'Marketing & Other'
+                    then 'Marketing & Other'
+                when reasons = 'Marketing'
+                    then 'Marketing'
+                when reasons = 'Promotion'
+                    then 'Promotion'                   
+            end as reasons
+        from {{ ref('int_reasons') }}
     )
 
-select * 
-from reason
+select *
+from reason_aggregate
